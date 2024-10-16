d4af96a785614e658eb9ee1d80ca7bbc3997a686
Alex <tajcortezz@gmail.com>
2024-10-16 21:01:39 +0300

    fix source main

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
-import { createRoot } from "react-dom/client";
 import "./index.css";
 import Router from "./Router/Router";
 import { Provider } from "react-redux";
 import { store } from "./store/store";
+import { createRoot } from "react-dom/client";
 
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
